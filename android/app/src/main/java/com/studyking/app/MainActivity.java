0f56119a26f48d18d4c478c345f2370b8226c918
chaurasiyaarzoo9-sudo <chaurasiyaarzoo9@gmail.com>
2026-09-22 16:06:10 +0530

    Create MainActivity.java

---
+++ android/app/src/main/java/com/studyking/app/MainActivity.java
@@ -0,0 +1,45 @@
+package com.studyking.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setSupportZoom(false);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(
+            "https://chaurasiyaarzoo9-sudo.github.io/Study-king-/"
+        );
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
